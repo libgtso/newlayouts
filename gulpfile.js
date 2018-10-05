5399b4ad828dc178a1a5e7f5f96edff1264f6841
Alexander <sir_mas@mail.ru>
2018-10-05 14:00:59 +0500

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,89 @@
+var gulp = require("gulp"),
+  sass = require("gulp-sass"),
+  browserSync = require("browser-sync"),
+  uglify = require("gulp-uglify"),
+  csso = require("gulp-csso"),
+  imagemin = require("gulp-imagemin"),
+  pngquant = require("imagemin-pngquant"),
+  sourcemaps = require("gulp-sourcemaps"),
+  htmlmin = require("gulp-htmlmin"),
+  nunjucks = require("gulp-nunjucks-html"),
+  autoprefixer = require("gulp-autoprefixer");
+
+var path = {
+  src: {
+    templates: "src/blocks"
+  }
+};
+
+gulp.task("sass", function() {
+  return gulp
+    .src("src/style/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(autoprefixer(["last 2 versions", "> 1%"], { cascade: true }))
+    .pipe(csso())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("./style"))
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task("scripts", function() {
+  return gulp
+    .src("src/js/*.js")
+    .pipe(sourcemaps.init())
+    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest("js"));
+});
+
+gulp.task("html", function() {
+  return gulp
+    .src("src/pages/*.html")
+    .pipe(
+      nunjucks({
+        searchPaths: [path.src.templates]
+      })
+    )
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("./"));
+});
+
+gulp.task("img", function() {
+  return gulp.src("./images/*.png").pipe(
+    imagemin({
+      interlaced: true,
+      progressive: true,
+      optimizationLevel: 7
+    })
+  );
+  use: [pngquant()].pipe(gulp.dest("./images"));
+});
+
+gulp.task("browser-sync", function() {
+  browserSync({
+    server: {
+      baseDir: "./"
+    },
+    notify: false,
+    // tunnel: true,
+    host: "localhost",
+    port: 9000,
+    logPrefix: "bkkb"
+  });
+});
+
+gulp.task(
+  "watch",
+  ["browser-sync", "sass", "scripts", "img", "html"],
+  function() {
+    gulp.watch("src/**/*.scss", ["sass"]);
+    gulp.watch("./*.html", browserSync.reload);
+    gulp.watch("src/js/*.js", function(event, cb) {
+      gulp.start("scripts", browserSync.reload);
+    });
+    gulp.watch("src/blocks/**/*.njk", function(event, cb) {
+      gulp.start("html", browserSync.reload);
+    });
+  }
+);
---
+++ src/js/main.js
@@ -0,0 +1,118 @@
+$(document).ready(function() {
+  showHide();
+  mainslider();
+  openimage();
+  textCut();
+  mapsCustom();
+  usefullslider();
+});
+
+function showHideBlock(e) {
+  e.preventDefault();
+
+  var block = $(this).closest(".sh-block");
+  block.toggleClass("sh-hidden");
+}
+
+function showHide() {
+  $(document).on("click", ".sh-btn", showHideBlock);
+  $(document).on("click", ".cls-btn", showHideBlock);
+}
+
+function mainslider() {
+  $(".mainslider").owlCarousel({
+    loop: true,
+    items: 1,
+    autoplay: false,
+    mouseDrag: false,
+    nav: true,
+    dots: false,
+    navText: false
+  });
+}
+
+function usefullslider() {
+  $(".usefullslider").owlCarousel({
+    loop: false,
+    items: 2,
+    autoplay: false,
+    mouseDrag: true,
+    nav: true,
+    dots: true,
+    navText: false,
+    slideBy: 1
+  });
+}
+
+function openimage() {
+  $(document).on("click", ".open-img", function(e) {
+    e.preventDefault();
+
+    var block = $(this).closest(".block");
+    var active = block.find("div.active").find("img");
+    var popup = block.find(".popup");
+
+    popup
+      .fadeIn()
+      .html($(active[0]).clone())
+      .append('<div class="popup-close"></div>');
+    closePopup();
+
+    function closePopup() {
+      $(document).on("click", ".popup-close", function(e) {
+        popup.fadeOut();
+      });
+    }
+  });
+}
+
+function textCut() {
+  var text = $(document).find(".news-preview p");
+  text.each(function(i, item) {
+    var size = 98;
+    var elementText = item.innerText;
+    //console.log(elementText.length);
+    if (elementText.length > size) {
+      var result = elementText.substring(0, size) + "...";
+      result.replace(/\s{2,}/g, " ");
+      //console.log(result);
+      item.innerText = result;
+    } else {
+      return item;
+    }
+  });
+}
+
+function mapsCustom() {
+  ymaps.ready(function() {
+    var myMap = new ymaps.Map(
+        "map",
+        {
+          center: [56.852071, 53.213458],
+          zoom: 17
+        },
+        {
+          searchControlProvider: "yandex#search"
+        }
+      ),
+      MyIconContentLayout = ymaps.templateLayoutFactory.createClass(
+        '<div style="color: #FFFFFF; font-weight: bold;">$[properties.iconContent]</div>'
+      );
+    myPlacemarkWithContent = new ymaps.Placemark(
+      [56.852071, 53.213458],
+      {
+        hintContent: "Лихвинцева, 46"
+      },
+      {
+        iconLayout: "default#imageWithContent",
+        iconImageHref: "./images/icon.png",
+        // iconImageSize: [48, 70],
+        // iconImageOffset: [-24, -64],
+        // iconContentOffset: [15, 15],
+        iconContentLayout: MyIconContentLayout
+      }
+    );
+
+    myMap.geoObjects.add(myPlacemarkWithContent);
+  });
+}
